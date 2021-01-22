9007b27a5e43581cec58a1b1422f43e1adf3eae9
Beth Ruff <beth.ruff2@gmail.com>
2021-01-21 23:22:50 -0800

    add are there duplicates solution using multiple pointers

--- areThereDuplicates.js
+++ areThereDuplicates.js
@@ -10,3 +10,19 @@ function areThereDuplicates() {
     }
     return false;
   }
+
+  //multiple pointers solution
+
+  function areThereDuplicates(...args) {
+    args.sort((a,b) => a > b);
+    let start = 0;
+    let next = 1;
+    while(next < args.length){
+      if(args[start] === args[next]){
+          return true
+      }
+      start++
+      next++
+    }
+    return false
+  }
